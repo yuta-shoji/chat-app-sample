de2173a502243001e752d583b80dcdf6f5024625
Yuta Shoji <shoshoshon.music@gmail.com>
2023-01-25 11:45:28 +0900

    Initial Commit

---
+++ chat-app-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat-app-sample
+//
+//  Created by Yuta Shoji on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
